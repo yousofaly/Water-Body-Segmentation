79cb4882577de2a3fa78f941cde2970a5e2db3ce
yousofaly <56979366+yousofaly@users.noreply.github.com>
2021-03-10 16:14:21 -0600

    Update generator.py

--- generator.py
+++ generator.py
@@ -5,7 +5,6 @@ def generator(df, ih = 1024, iw = 1024, start = 0, stop = 4, bs = 32,
               aug = None, interp = cv2.INTER_NEAREST, rs = 255):
     images = []
     masks = []
-    h,w = df['height'], df['width']
     while True:
         for i in range(start,stop):
             images.append(cv2.resize(cv2.imread(df['image'][i]), (ih,iw), interpolation = interp) / rs)
