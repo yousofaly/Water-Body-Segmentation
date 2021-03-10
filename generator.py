c21740c7a9dc722b4f34db9a80c883cec652ae89
yousofaly <56979366+yousofaly@users.noreply.github.com>
2021-03-10 11:09:18 -0600

    Update generator.py

--- generator.py
+++ generator.py
@@ -2,14 +2,14 @@ import cv2
 
 #image generator from files df
 def generator(df, ih = 1024, iw = 1024, start = 0, stop = 4, bs = 32, 
-              aug = None, interp = cv2.INTER_NEAREST):
+              aug = None, interp = cv2.INTER_NEAREST, rs = 255):
     images = []
     masks = []
     h,w = df['height'], df['width']
     while True:
         for i in range(start,stop):
-            images.append(cv2.resize(cv2.imread(df['image'][i]), (ih,iw), interpolation = interp))
-            masks.append(cv2.resize(cv2.imread(df['mask'][i]), (ih,iw), interpolation = interp))
+            images.append(cv2.resize(cv2.imread(df['image'][i]), (ih,iw), interpolation = interp) / rs)
+            masks.append(cv2.resize(cv2.imread(df['mask'][i]), (ih,iw), interpolation = interp) / rs)
             
             if len(masks) == bs:
                 if aug is not None:
