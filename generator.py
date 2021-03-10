c71529277a5ab03b1c337dcc0502a8906f1ce527
yousofaly <56979366+yousofaly@users.noreply.github.com>
2021-03-10 09:58:05 -0600

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,16 +1,16 @@
-from scipy.ndimage import zoom
 import cv2
 
 #image generator from files df
-def generator(df, ih = 1024, iw = 1024, start = 0, stop = 4, bs = 32, aug = None):
+def generator(df, ih = 1024, iw = 1024, start = 0, stop = 4, bs = 32, 
+              aug = None, interp = cv2.INTER_NEAREST):
     images = []
     masks = []
     h,w = df['height'], df['width']
     while True:
         for i in range(start,stop):
-            images.append(zoom(cv2.imread(df['image'][i]), (ih/h[i], iw/w[i],1)))
-            masks.append(zoom(cv2.imread(df['mask'][i]), (ih/h[i], iw/w[i],1)))
-        
+            images.append(cv2.resize(cv2.imread(df['image'][i]), (ih,iw), interpolation = interp))
+            masks.append(cv2.resize(cv2.imread(df['mask'][i]), (ih,iw), interpolation = interp))
+            
             if len(masks) == bs:
                 if aug is not None:
                     pass
