43194f506b0110d60a79e7f720286fb8f4055a97
yousofaly <56979366+yousofaly@users.noreply.github.com>
2021-03-14 21:58:47 -0500

    Update and rename crop.py to processing.py

--- processing.py
+++ processing.py
@@ -1,5 +1,5 @@
 import cv2
-
+import os 
 #define cropping function 
 def crop(path, mode = 'dims'):
 
@@ -24,3 +24,14 @@ def crop(path, mode = 'dims'):
     #get cropped image
     else:
         return(cv2.imread(path)[y:y+h,x:x+w,:])
+
+#define function to get paths from data folder
+def get_paths():
+  image_paths = []
+  mask_paths = []
+  for p in sorted(os.listdir('Data/Images')):
+    image_paths.append(os.path.join('Data','Images',p))
+  for p in sorted(os.listdir('Data/Masks'):
+    mask_paths.append(os.path.join('Data','Masks',p))
+  
+  return (image_paths, mask_paths)
