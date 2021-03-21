5b4067352c437a53769d3d5fea11ccb05113b085
yousofaly <56979366+yousofaly@users.noreply.github.com>
2021-03-21 17:35:38 -0500

    Update processing.py

--- processing.py
+++ processing.py
@@ -25,24 +25,3 @@ def crop(path, mode = 'dims'):
     #get cropped image
     else:
         return(cv2.imread(path)[y:y+h,x:x+w,:])
-
-#define function to get paths from data folder
-def get_paths():
-  image_paths = []
-  mask_paths = []
-  for p in sorted(os.listdir('Data/Images')):
-    image_paths.append(os.path.join('Data','Images',p))
-  for p in sorted(os.listdir('Data/Masks')):
-    mask_paths.append(os.path.join('Data','Masks',p))
-  
-  return (image_paths, mask_paths)
-                  
-#custom metric and loss function 
-def dice_coef(y_true, y_pred):
-    y_true_f = K.flatten(y_true)
-    y_pred_f = K.flatten(y_pred)
-    intersection = K.sum(y_true_f * y_pred_f)
-    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
-
-def dice_coef_loss(y_true, y_pred):
-  return (1-dice_coef(y_true, y_pred))
